72d2f184bea04f630a4b529347312a9b6d31d573
luka onik <luka.onikadze@toptal.com>
2024-02-05 14:29:21 +0400

    build: initialize project
    initialize project. add husky,commitlint, playground and ng-king library

---
+++ projects/ng-king/src/lib/ng-king.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng-king',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ng-king works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgKingComponent {
+
+}
---
+++ projects/ng-king/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-king
+ */
+
+export * from './lib/ng-king.service';
+export * from './lib/ng-king.component';
