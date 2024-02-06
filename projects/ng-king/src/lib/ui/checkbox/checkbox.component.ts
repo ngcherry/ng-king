0acb1adbd7385afe436b3ae1644a99c334c89aeb
luka onik <luka.onikadze@toptal.com>
2024-02-06 21:58:28 +0400

    feat: build basic checkbox component

---
+++ projects/ng-king/src/lib/ui/checkbox/checkbox.component.ts
@@ -0,0 +1,78 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  HostListener,
+  computed,
+  effect,
+  forwardRef,
+  input,
+  signal,
+} from '@angular/core';
+import { KNIconComponent } from '../icon/icon.component';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'kn-checkbox',
+  standalone: true,
+  imports: [KNIconComponent],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => KNCheckboxComponent),
+    },
+  ],
+})
+export class KNCheckboxComponent implements ControlValueAccessor {
+  private _onChange!: (_: any) => void;
+  private _onTouched!: () => void;
+  public disabled = signal(false);
+  value = signal<boolean | undefined | null>(false);
+
+  @HostBinding('class.label-before-align')
+  beforeAlign!: boolean;
+
+  @HostBinding('class.disabled')
+  isDisabled = false;
+
+  constructor() {
+    effect(() => {
+      this.beforeAlign = this.labelPosition() === 'before';
+    });
+
+    effect(() => {
+      this.isDisabled = this.disabled();
+    });
+  }
+
+  labelPosition = input<'before' | 'after'>('after');
+
+  align = input<'center' | 'start'>('center');
+
+  @HostListener('click')
+  onClick() {
+    if (this.disabled()) {
+      return;
+    }
+    this.value.set(!this.value());
+    this._onChange(this.value());
+    this._onTouched();
+  }
+
+  writeValue(obj: boolean | undefined | null): void {
+    this.value.set(obj);
+  }
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+}
--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   KNIconComponent,
   KNIconRegistry,
 } from '../../../ng-king/src/public-api';
+import { KNCheckboxComponent } from '../../../ng-king/src/lib/ui/checkbox/checkbox.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, KNIconComponent],
+  imports: [RouterOutlet, KNIconComponent, KNCheckboxComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -17,15 +19,15 @@ export class AppComponent {
 
   iconName = signal('checkbox');
 
+  checkboxValue = false;
+
   constructor() {
+    console.log('ROOT');
+
     this._iconRegistry.addIcon('checkbox', 'assets/checkbox.svg');
     this._iconRegistry.addIcon(
       'checkbox-checked',
       'assets/checkbox-checked.svg'
     );
-
-    setTimeout(() => {
-      this.iconName.set('checkbox-checked');
-    }, 1000);
   }
 }
