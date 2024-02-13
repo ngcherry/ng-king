6775e6688a339c5801d983926f18dea6593f88f2
luka onik <luka.onikadze@toptal.com>
2024-02-13 21:32:30 +0400

    feat: move to carbon design

--- projects/ng-king/src/lib/ui/button/button.component.ts
+++ projects/ng-king/src/lib/ui/button/button.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
+  ViewEncapsulation,
   computed,
   input,
   signal,
@@ -9,18 +10,36 @@ import {
 import { KNSpinnerComponent } from '../spinner/spinner.component';
 import { NgClass } from '@angular/common';
 
+type BUTTON_SIZES = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+
 @Component({
-  selector: 'kn-button',
+  selector: '[cds-button]',
   standalone: true,
   imports: [KNSpinnerComponent, NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'cds-button cds-button-primary',
+  },
+  encapsulation: ViewEncapsulation.None,
 })
 export class KNButtonComponent {
   loading = input(false);
 
   disabled = input<boolean>();
+
+  @HostBinding('attr.disabled')
+  get disabledAttr() {
+    return this.disabled();
+  }
+
+  @HostBinding('class')
+  get buttonClasses() {
+    return `cds-button-${this.size()}`;
+  }
+
+  size = input<BUTTON_SIZES>('lg');
 
   color = input<string>();
 
